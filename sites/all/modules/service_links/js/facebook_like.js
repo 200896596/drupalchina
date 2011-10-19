813804bb9b81792cb774bdfe365f223883fa3a15
Wang Tianjiao <wtj007@gmail.com>
2011-10-19 22:25:17 +0200

    add modules

---
+++ sites/all/modules/service_links/js/facebook_like.js
@@ -0,0 +1,8 @@
+(function ($) {
+  $(document).ready(function(){
+    $('a.service-links-facebook-like').each(function(){
+      iframe_txt='<iframe src="' + $(this).attr('href') + '" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:' + Drupal.settings.ws_fl.width + 'px; height:' + Drupal.settings.ws_fl.height + 'px;"' + ' allowTransparency="true"></iframe>';
+      $(this).replaceWith(iframe_txt);
+    });
+  });
+})(jQuery);
